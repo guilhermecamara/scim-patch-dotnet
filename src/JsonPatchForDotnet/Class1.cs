f321bb0c269b723298dba45bc1a7121dfa908b77
Guilherme Câmara <guilherme.camara@outlook.com.br>
2024-07-24 10:26:34 -0300

    feat: initial implementation of an object path resolver

---
+++ src/JsonPatchForDotnet/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JsonPatchForDotnet
+{
+    public static class PathResolver
+    {
+        public static IEnumerable<object> GetProperties(this object o, string[] paths)
+        {
+            var objects = new List<object>();
+
+            var path = paths[0];
+            var children = GetProperties(o, path);
+            
+            if (paths.Length > 1)
+            {
+                var childPaths = paths.Skip(1).ToArray();
+                foreach (var child in children)
+                {
+                    if (IsIEnumerable(child.GetType()))
+                    {
+                        foreach (var item in (IEnumerable)child)
+                        {
+                            objects.AddRange(GetProperties(item, childPaths));
+                        }
+                    }
+                    else
+                    {
+                        objects.AddRange(GetProperties(child, childPaths));
+                    }
+                }
+            }
+            else
+            {
+                objects.AddRange(children);
+            }
+
+            return objects;
+        }
+        
+        public static IEnumerable<object> GetProperties(this object o, string path)
+        {
+            var (root, filter) = GetRootPath(path);
+
+            var type = o.GetType();
+            
+            if (IsIEnumerable(type))
+            {
+                // TODO apply filter
+
+                foreach (var item in (IEnumerable<object>)o)
+                {
+                    yield return item.GetType().GetProperty(root)!.GetValue(item);
+                }
+            }
+            else
+            {
+                yield return o.GetType().GetProperty(root)!.GetValue(o);
+            }
+        }
+
+        private static (string, string?) GetRootPath(string path)
+        {
+            var root = path.Split('[');
+            return (root.ElementAt(0), root.ElementAtOrDefault(1));
+        }
+
+        private static bool IsIEnumerable(Type type)
+        {
+            return type.IsGenericType &&
+                   typeof(IEnumerable).IsAssignableFrom(type.GetGenericTypeDefinition());
+        }
+        
+        private static bool IsIList(Type type)
+        {
+            return type.IsGenericType &&
+                   typeof(IList).IsAssignableFrom(type.GetGenericTypeDefinition());
+        }
+    }
+}
